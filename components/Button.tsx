f24b492f4397bc967f8bcf8fc47c557467fca7d3
johnn <johnf@risk.nl>
2025-05-08 21:43:03 +0300

    Started mobile refactoring

--- components/Button.tsx
+++ components/Button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
 export const Button = (props: ButtonProps) => {
   return (
     <button onClick={props.onClick}
-      className="bg-transparent hover:bg-stone-700 w-50 text-brown-700 font-semibold hover:text-white py-2 px-4 border border-brown-700 hover:border-transparent rounded cursor-pointer">
+      className="bg-transparent hover:bg-stone-700 text-brown-700 font-semibold hover:text-white py-2 px-4 border border-brown-700 hover:border-transparent rounded cursor-pointer">
       {props.children}
     </button>
   )
