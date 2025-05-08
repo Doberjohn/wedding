b3b7534bee356162cdd3e9316aadf42b1d995cb3
johnn <johnf@risk.nl>
2025-05-08 20:51:54 +0300

    Started mobile refactoring

--- app/page.tsx
+++ app/page.tsx
@@ -1,76 +1,37 @@
 "use client"
-import {ImageGallery} from "react-image-grid-gallery";
-import {OurHistory} from "@/components/OurHistory";
+// import {ImageGallery} from "react-image-grid-gallery";
+// import {OurHistory} from "@/components/OurHistory";
 import {Header} from "@/components/Header";
-import {Schedule} from "@/components/Schedule";
-import {Divider} from "@/components/Divider";
-import {Photoshare} from "@/components/Photoshare";
-import {Countdown} from "@/components/Countdown";
+// import {Schedule} from "@/components/Schedule";
+// import {Divider} from "@/components/Divider";
+// import {Photoshare} from "@/components/Photoshare";
+// import {Countdown} from "@/components/Countdown";
+// import {images} from "@/config/images";
 
 export default function Home() {
-
-  const imagesArray = [
-    {
-      id: "couple_photo_3",
-      alt: "Image1's alt text",
-      caption: "Image1's description",
-      src: "/couple_photo_1.webp",
-    },
-    {
-      id: "couple_photo_3",
-      alt: "Image1's alt text",
-      caption: "Image1's description",
-      src: "couple_photo_2.webp",
-    },
-    {
-      id: "couple_photo_3",
-      alt: "Image1's alt text",
-      caption: "Image1's description",
-      src: "couple_photo_3.webp",
-    },
-    {
-      id: "couple_photo_4",
-      alt: "Image1's alt text",
-      caption: "Image1's description",
-      src: "couple_photo_4.webp",
-    },
-    {
-      id: "couple_photo_5",
-      alt: "Image1's alt text",
-      caption: "Image1's description",
-      src: "couple_photo_5.webp",
-    },
-    {
-      id: "couple_photo_6",
-      alt: "Image1's alt text",
-      caption: "Image1's description",
-      src: "couple_photo_6.webp",
-    }
-  ];
-
   return (
     <div
-      className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      className="grid items-center justify-items-center font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[50px] row-start-2 items-center text-center">
         <Header/>
 
-        <div className="w-[70%]">
-          <ImageGallery imagesInfoArray={imagesArray} gapSize={10} columnCount={3}/>
-        </div>
+        {/*<div className="w-[70%]">*/}
+        {/*  <ImageGallery imagesInfoArray={images} gapSize={10} columnCount={3}/>*/}
+        {/*</div>*/}
 
-        <OurHistory/>
-        <Divider/>
+        {/*<OurHistory/>*/}
+        {/*<Divider/>*/}
 
-        <Schedule/>
-        <Divider/>
+        {/*<Schedule/>*/}
+        {/*<Divider/>*/}
 
-        <Photoshare/>
-        <Divider/>
+        {/*<Photoshare/>*/}
+        {/*<Divider/>*/}
 
-        <Countdown/>
+        {/*<Countdown/>*/}
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <div className="text-sm italic">
+        <div className="w-[60%] md:w-[100%] text-sm italic text-center">
           Η σελίδα του γάμου μας φτιάχτηκε με αγάπη από εμάς
         </div>
       </footer>
---
+++ config/images.ts
@@ -0,0 +1,38 @@
+export const images = [
+  {
+    id: "couple_photo_3",
+    alt: "Image1's alt text",
+    caption: "Image1's description",
+    src: "/couple_photo_1.webp",
+  },
+  {
+    id: "couple_photo_3",
+    alt: "Image1's alt text",
+    caption: "Image1's description",
+    src: "couple_photo_2.webp",
+  },
+  {
+    id: "couple_photo_3",
+    alt: "Image1's alt text",
+    caption: "Image1's description",
+    src: "couple_photo_3.webp",
+  },
+  {
+    id: "couple_photo_4",
+    alt: "Image1's alt text",
+    caption: "Image1's description",
+    src: "couple_photo_4.webp",
+  },
+  {
+    id: "couple_photo_5",
+    alt: "Image1's alt text",
+    caption: "Image1's description",
+    src: "couple_photo_5.webp",
+  },
+  {
+    id: "couple_photo_6",
+    alt: "Image1's alt text",
+    caption: "Image1's description",
+    src: "couple_photo_6.webp",
+  }
+]
